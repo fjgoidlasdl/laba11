ab7cf1db43b4427b42d89651cdbec3dbe0beb95f
fjgoidlasdl <timoshenkoegor2281@gmail.com>
2026-09-12 09:28:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.end('<h1>Привет, мир!</h1>');
+  res.end('<h1>Тимошенко Егор Иванович</h1>');
 });
 
 server.listen(3000, () => {
